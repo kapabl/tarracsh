b063ea82a434015a320f9cf0ce3064c113e13ae2
kapa <kapablanka2127@gmail.com>
2022-07-31 05:37:26 -0400

    Logging

--- src/classfile/Log.h
+++ src/classfile/Log.h
@@ -13,19 +13,25 @@ public:
     void write(const std::string &value ) {
         assert(!_logFile.empty());
         std::unique_lock<std::mutex> lock(_mutex);
-        std::ofstream file(_logFile);
+        std::ofstream file(_logFile, std::ios_base::app);
         file << value;
     }
 
     void writeln(const std::string& value) {
         assert(!_logFile.empty());
         std::unique_lock<std::mutex> lock(_mutex);
-        std::ofstream file(_logFile);
+        std::ofstream file(_logFile, std::ios_base::app);
         file << value << std::endl;
     }
 
     void setFile(std::string logFile) {
         _logFile = std::move(logFile);
+    }
+    
+    static void emptyLogFile(const std::string& file) {
+        std::ofstream logFile;
+        logFile.open(file, std::ofstream::out | std::ofstream::trunc);
+        logFile.close();
     }
 
 private:
