ba7bf1ed1862977044c552ff9ba24bf743fa767b
kapa <kapablanka2127@gmail.com>
2023-05-13 02:27:10 -0400

    classfile diff CLI. Re-using tarracsh

--- cfdiff/src/cli/ClassfileDiffApp.h
+++ cfdiff/src/cli/ClassfileDiffApp.h
@@ -11,21 +11,29 @@ namespace kapa::cldiff::app {
 class ClassfileDiffApp final : public infrastructure::app::cli::CliApp {
 
 public:
-    [[nodiscard]] infrastructure::app::cli::ExitCode start(int argc, char *argv[]);
-
     static infrastructure::app::cli::ExitCode run(int argc, char **argv);
 
 private:
+    void setupCliOptions();
+    void validateFile(const std::string &file) const;
+    infrastructure::app::cli::ExitCode parseCli(int argc, char** argv);
     static std::unique_ptr<ClassfileDiffApp> _app;
-    std::shared_ptr<kapa::infrastructure::log::Log> _log;
+    std::shared_ptr<infrastructure::log::Log> _log;
     tarracsh::domain::Options _options;
 
     tarracsh::domain::stats::Results _results;
+    bool _ignoreClassname{false};
+    bool _publicOnly{true};
+
+    std::string _leftFile;
+    std::string _rightFile;
 
     void init() const;
 
     ClassfileDiffApp(const std::string &description, const std::string &name,
         std::shared_ptr<infrastructure::log::Log> log);
+
+    [[nodiscard]] infrastructure::app::cli::ExitCode start(int argc, char* argv[]);
 
 
 };
