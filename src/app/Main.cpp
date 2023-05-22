93d7eaedc57ca0b04ad6e0edc149aa17cb585cc6
kapa <kapablanka2127@gmail.com>
2023-05-21 23:00:39 -0400

    CMake: adding tarracsh target

--- src/app/Main.cpp
+++ src/app/Main.cpp
@@ -1,11 +1,28 @@
 #include "App.h"
 
 
-// #ifndef _DEBUG
-#pragma comment(linker, "/include:__tcmalloc")
-#pragma comment(lib, "libtcmalloc_minimal.lib")
-// #endif
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma clang diagnostic ignored "-Wpragma-pack"
+#endif
+
+extern "C" {
+__attribute__((weak)) void __tcmalloc();
+}
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+#ifdef _MSC_VER
+    // #ifndef _DEBUG
+    #pragma comment(linker, "/include:__tcmalloc")
+//    #pragma comment(lib, "libtcmalloc_minimal.lib")
+    // #endif
+#endif
 
 using namespace kapa::tarracsh;
 using namespace app;
